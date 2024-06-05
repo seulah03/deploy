b75f6be3685a975e006cbc1a012f3e276909e503
seulah03 <sarhee1004@naver.com>
2024-06-05 18:21:29 +0900

    deploytest

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import UseStore from "../store/store.js";
+
+const Counter = () => {
+  const { count, increase, decrease } = useStore();
+
+  return (
+    <div>
+      <h1>Count; {count}</h1>
+      <button onClick={increase}>증가</button>
+      <button onClick={decrease}>감소</button>
+    </div>
+  );
+};
+
+export default Counter;
